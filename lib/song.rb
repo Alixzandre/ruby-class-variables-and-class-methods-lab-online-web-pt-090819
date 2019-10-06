5a8e5c9a3b8bed2b975d31e5bc82ffa748d784bd
Alexander Bispo <alexbispo173@gmail.com>
2019-10-06 18:54:07 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -17,6 +17,17 @@ class Song
   
   def self.genre_count
     genre_count = {}
+    
+    genre_count = {}
+    @@genres.each do |genre|
+      if genre_count[genre]
+        genre_count[genre] += 1 
+      else
+        genre_count[genre] = 1
+      end
+    end
+    genre_count
+    
   end
   
   def self.artist_count
