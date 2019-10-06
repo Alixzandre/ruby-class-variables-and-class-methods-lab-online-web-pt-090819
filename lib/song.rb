9f3470acd3f0e6432f7467297497a02ae789d1ab
Alexander Bispo <alexbispo173@gmail.com>
2019-10-06 18:45:55 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,22 @@
+class Song
+  attr_accessor :name, :artist, :genre
+  @@count = 0
+  @@genres = []
+  
+  def self.genres
+    @@genres.uniq
+  
+  def self.count
+    @@count
+  end
+  
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+  end
+ 
+  
+  
+end
